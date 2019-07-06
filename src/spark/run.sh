28e2e3a08b9459ebe27844f42dc015db1fbc6e42
eddiewu5300 <bennice50@gmail.com>
2019-07-06 19:10:30 -0400

    update readme

--- src/spark/run.sh
+++ src/spark/run.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 spark-submit \
-    --conf spark.cassandra.connection.host="10.0.0.13" \
+    --conf spark.cassandra.connection.host="10.0.0.13, 10.0.0.7" \
     --master spark://ip-10-0-0-11:7077 \
     --conf spark.executor.memoryOverhead=600 \
     --executor-memory 5G \
